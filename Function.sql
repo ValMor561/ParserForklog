b0f418226f97c33aca0d682ddde770b8ffe539ad
ValMor561 <koli1babenko2@yandex.ru>
2024-03-26 13:14:07 +0300

    add table define

--- Function.sql
+++ Function.sql
@@ -1,7 +1,10 @@
-CREATE OR REPLACE FUNCTION check_url_exists(p_url text)
+CREATE OR REPLACE FUNCTION check_url_exists(p_url text, p_tablename text)
 RETURNS BOOLEAN
 AS $$
+DECLARE
+    url_exists BOOLEAN;
 BEGIN
-    RETURN EXISTS(SELECT 1 FROM public."Visited" WHERE url = p_url);
+    EXECUTE format('SELECT EXISTS(SELECT 1 FROM %I WHERE url = $1)', p_tablename) INTO url_exists USING p_url;
+    RETURN url_exists;
 END;
 $$ LANGUAGE plpgsql;
